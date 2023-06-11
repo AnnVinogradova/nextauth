46e76e04fd9f0293cfda4ad9d6f340570eecf664
AnnVinogradova <saikotakaki@gmail.com>
2023-06-11 09:15:09 +0300

    ---

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Home() {
   const { data: session } = useSession();
-  if(session) {
+  if(session && session.user) {
     return <>
       Signed in as {session.user.email} <br/>
       <button onClick={() => signOut()}>Sign out</button>
