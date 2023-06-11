5668d1a8bfc659201a49120615b3c65ca2919a8c
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2023-06-11 10:15:26 +0400

    небольшие изменения чтоб линтинг проходил

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,30 +2,33 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const authOptions = {
-	providers: [
-		CredentialsProvider({
-			name: 'Credentials',
-			credentials: {
-				username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-				password: { label: 'Password', type: 'password' }
-			},
-			async authorize(credentials, req) {
-				console.log('credentials', credentials);
+  providers: [
+    // eslint-disable-next-line new-cap
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' }
+      },
+      // eslint-disable-next-line no-unused-vars
+      async authorize(credentials, req) {
+        console.log('credentials', credentials);
 
 
-				if ('111' === credentials.username && '111' === credentials.password)
-					return { id: '1', name: 'J Smith', email: 'jsmith@example.com' };
-				return null;
-			}
-		})
-	],
+        if ('111' === credentials.username && '111' === credentials.password)
+          return { id: '1', name: 'J Smith', email: 'jsmith@example.com' };
+        return null;
+      }
+    })
+  ],
 };
 
+// eslint-disable-next-line new-cap
 const resf = NextAuth(authOptions);
 
 export default (...params) => {
-	const [req] = params;
-	console.log('pages/api/auth/[...nextauth].js ');
-	console.log('>> ', req.method, ' запрос на', req.url, req.query);
-	return resf(...params);
+  const [req] = params;
+  console.log('pages/api/auth/[...nextauth].js ');
+  console.log('>> ', req.method, ' запрос на', req.url, req.query);
+  return resf(...params);
 };
