1e6ff9435b3072abbe218c8fae28e0aa598d6edc
AnnVinogradova <saikotakaki@gmail.com>
2023-06-14 13:39:39 +0300

    VK работает

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GitHubProvider from "next-auth/providers/github";
 import VkProvider from "next-auth/providers/vk";
+import SpotifyProvider from "next-auth/providers/spotify";
 // import { PrismaAdapter } from "@auth/prisma-adapter";
 // import { PrismaClient } from "@prisma/client";
 
@@ -13,6 +14,11 @@ export const authOptions = {
 			clientId: process.env.GITHUB_ID,
 			clientSecret: process.env.GITHUB_SECRET
 		}),
+
+		SpotifyProvider({
+			clientId: process.env.SPOTIFY_CLIENT_ID,
+			clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+		  }),
 
 		VkProvider({
 			clientId: process.env.VK_CLIENT_ID,
