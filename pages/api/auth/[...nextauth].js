bc759a869237148cfc24c0eb20aaf3c12852151a
AnnVinogradova <saikotakaki@gmail.com>
2023-06-15 14:26:07 +0300

    prisma

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GitHubProvider from "next-auth/providers/github";
 import VkProvider from "next-auth/providers/vk";
+
 // import { PrismaAdapter } from "@auth/prisma-adapter";
 // import { PrismaClient } from "@prisma/client";
 
